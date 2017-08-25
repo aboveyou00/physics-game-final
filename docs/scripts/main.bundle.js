8009d96e9d3a8b6e50f6cc9b62372a82462910d5
Brandon <brandonyoyoslade@gmail.com>
2017-08-25 14:19:05 -0600

    feat(game): add keyboard and gamepad menu input

--- docs/scripts/main.bundle.js
+++ docs/scripts/main.bundle.js
@@ -5552,12 +5552,14 @@ kbProvider.bindAbstractButton('left', 'ArrowLeft', 'KeyA');
 kbProvider.bindAbstractButton('right', 'ArrowRight', 'KeyD');
 kbProvider.bindAbstractButton('up', 'ArrowUp', 'KeyW');
 kbProvider.bindAbstractButton('down', 'ArrowDown', 'KeyS');
+kbProvider.bindAbstractButton('select', 'Enter', 'Space');
 var gpProvider = new engine_1.GamepadAbstractButtonProvider(game.eventQueue);
 game.eventQueue.addAbstractButtonProvider(gpProvider);
 gpProvider.bindAbstractButton('left', 'DPadLeft', 'LeftStickLeft');
 gpProvider.bindAbstractButton('right', 'DPadRight', 'LeftStickRight');
 gpProvider.bindAbstractButton('up', 'DPadUp', 'LeftStickUp');
 gpProvider.bindAbstractButton('down', 'DPadDown', 'LeftStickDown');
+gpProvider.bindAbstractButton('select', 'A');
 
 
 /***/ }),
--- game/src/main.browser.ts
+++ game/src/main.browser.ts
@@ -12,6 +12,8 @@ kbProvider.bindAbstractButton('right', 'ArrowRight', 'KeyD');
 kbProvider.bindAbstractButton('up', 'ArrowUp', 'KeyW');
 kbProvider.bindAbstractButton('down', 'ArrowDown', 'KeyS');
 
+kbProvider.bindAbstractButton('select', 'Enter', 'Space');
+
 let gpProvider = new GamepadAbstractButtonProvider(game.eventQueue);
 game.eventQueue.addAbstractButtonProvider(gpProvider);
 
@@ -19,3 +21,5 @@ gpProvider.bindAbstractButton('left', 'DPadLeft', 'LeftStickLeft');
 gpProvider.bindAbstractButton('right', 'DPadRight', 'LeftStickRight');
 gpProvider.bindAbstractButton('up', 'DPadUp', 'LeftStickUp');
 gpProvider.bindAbstractButton('down', 'DPadDown', 'LeftStickDown');
+
+gpProvider.bindAbstractButton('select', 'A');
